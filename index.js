fea17dab9ff7263e37e0dd8d06a57886f55bc200
Theodor Vararu <theo@vararu.org>
2019-09-29 12:49:16 +0100

    Add gzip compression

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const compression = require("compression");
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+
+app.use(compression());
 
 app.use(express.static(__dirname + "/public"));
 
